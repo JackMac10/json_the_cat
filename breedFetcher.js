f9beb3004af9dba98ad9b55440b0f3bdc25870a6
JackMac10 <macdonaldjackson0@gmail.com>
2024-03-19 23:17:49 -0700

    tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,29 @@
 const request = require("request");
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-// Check if breedName is provided
-if (!breedName) {
-  console.error('Please provide a breed name.');
-  process.exit(1);
-}
-const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error fetching data:', error);
+      return;
+    }
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching data:', error);
-    return;
-  }
-  
-  if (response.statusCode !== 200) {
-    console.error('Unexpected status code:', response.statusCode);
-    return;
-  }
-  
-  const data = JSON.parse(body);
-  
-  if (Array.isArray(data) && data.length > 0) {
-    const firstBreed = data[0];
-    
-    console.log(`${firstBreed.name}: ${firstBreed.description}`);
-  } else {
-    console.log(`Cat Breed "${breedName}" not found in database.`);
-  }
-});
+
+    if (response.statusCode !== 200) {
+      console.error('Unexpected status code:', response.statusCode);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    if (Array.isArray(data) && data.length > 0) {
+      const firstBreed = data[0];
+      callback(null, `${firstBreed.name}: ${firstBreed.description}`);
+    } else {
+      callback(null, `Breed "${breedName}" not found.`);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
